fcde1fbac95411886fd1be22e3ccc2d4a61473a6
frustrated-coder <d.n_mwangi@outlook.com>
2022-09-17 00:14:31 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return `${str[0].toUpperCase()}${str.slice(1)}`
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, "")
+  }
+  static titleize(str){
+    return str.replace(/\w+/g, (match, startIndex) =>{
+      const dontCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+      
+      return dontCapitalize.includes(match) && startIndex ? match : `${match[0].toUpperCase()}${match.slice(1)}`
+    })
+  }
 }
